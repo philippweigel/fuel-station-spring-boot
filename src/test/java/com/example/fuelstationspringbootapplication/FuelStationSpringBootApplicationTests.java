3bcfdfbd49e60ef03b07c67e63cfb5c845eb1e30
Philipp Weigel <philipp.weigel@hotmail.com>
2023-06-03 13:05:45 +0200

    Created project

---
+++ src/test/java/com/example/fuelstationspringbootapplication/FuelStationSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fuelstationspringbootapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelStationSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
